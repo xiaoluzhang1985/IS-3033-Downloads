6fc7a419059a5debbe21504809339373f9e7b312
XIAOLU ZHANG <xiaoluzhang1985@gmail.com>
2019-02-15 14:27:45 -0600

    Update lab03.c

--- lab03.c
+++ lab03.c
@@ -11,14 +11,14 @@ int main ()
  if (fpid < 0){ 
  	fprintf(stderr,"fork failed!\n");   
  }else if (fpid == 0) {  
- 	printf("i am the child process.");   
+ 	printf("i am the child process, my process id is %d\n",getpid());    
 	count++; 
  	while(1){
 		//infinite loop
 	} 
  }   
  else {   
-	printf("i am the parent process.");     
+	printf("i am the parent process, my process id is %d\n",getpid());    
 	count++;
 	while(1){
 		//infinite loop
